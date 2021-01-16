aa36718537677410bb61e215db4695231f71f5a6
H. Joe Lee <hyoklee@hdfgroup.org>
2021-01-15 19:41:12 -0600

    OESS-133:Fixed test scripts. Note that static h5dump doesn't work for Bitgroom. Use h5dump-shared in testCM.sh.

--- BITGROOM/example/test/testCM.sh
+++ BITGROOM/example/test/testCM.sh
@@ -205,7 +205,7 @@ REPACKTEST()
     # Run test.
     TESTING H5REPACK $@
     (
-        $ENVCMD $H5REPACK "$@" "out-$infile" "out-$outfile"
+        $ENVCMD $H5REPACK "$@" "$infile" "out-$outfile"
     ) >$actual 2>$actual_err
     RET=$?
     STDOUT_FILTER $actual
@@ -252,7 +252,7 @@ EXETEST h5ex_d_bitgroom h5ex_d_bitgroom.tst
 
 DUMPTEST h5ex_d_bitgroom.ddl h5ex_d_bitgroom.h5
 
-REPACKTEST h5repack_layout.h5 out-ud_convert.h5repack_layout.h5 -v -f UD=32022,5,3,4,0,0,0
+REPACKTEST h5repack_layout.h5 ud_convert.h5repack_layout.h5 -v -f UD=32022,1,5,3,4,0,0,0
 
 DUMPTEST h5repack_layout.h5-ud_convert.ddl -pH out-ud_convert.h5repack_layout.h5
 
