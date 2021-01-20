8cb0cc03cb8c0f60bd6d46180102295c01b862cc
H. Joe Lee <hyoklee@hdfgroup.org>
2021-01-19 19:59:00 -0600

    HDFFV-11207:Add example test script for all filters.

---
+++ bin/test.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/tcsh
+set path=(/scr/hyoklee/src/spack-hyoklee/bin/ $path)
+source /scr/hyoklee/src/spack-hyoklee/share/spack/setup-env.csh
+spack load hdf5-cmake
+set p="`spack find --paths hdf5-cmake | tail  -1 | cut -d' ' -f 3-`"
+setenv HDF5_HOME $p
+set pl="/scr/hyoklee/src/hdf5_plugins"
+
+# Most static tests will fail for static h5repack.
+cd $pl/BITGROOM/example/test/
+rm h5ex* *.h5 *.out* *.err *.dll 
+# ./test.sh
+./testCM.sh
+
+cd $pl/BLOSC/example/test/
+rm h5ex* *.h5 *.out* *.err *.dll 
+# ./test.sh
+./testCM.sh
+
+cd $pl/BSHUF/example/test/
+rm h5ex* *.h5 *.out* *.err *.dll 
+# ./test.sh
+./testCM.sh
+
+cd $pl/BZIP2/example/test/
+rm h5ex* *.h5 *.out* *.err *.dll 
+./test.sh
+./testCM.sh
+
+# cd $pl/JPEG/example/test/
+#rm h5ex_d_jpeg* *.h5 *.out* *.err *.dll 
+# ./test.sh
+# ./testCM.sh
+
+# cd $pl/LZ4/example/test/
+# ./test.sh
+# ./testCM.sh
+
+# cd $pl/LZF/example/test/
+# ./test.sh
+# ./testCM.sh
+
+# cd $pl/MAFISC/example/test/
+# ./test.sh
+# ./testCM.sh
+
+# cd $pl/SZF/example/test/
+# ./test.sh
+# ./testCM.sh
+
+# cd $pl/ZFP/example/test/
+# ./test.sh
+# ./testCM.sh
+
+# cd $pl/ZSTD/example/test/
+# ./test.sh
+# ./testCM.sh
